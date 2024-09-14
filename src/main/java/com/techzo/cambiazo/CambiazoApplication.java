c521d06382f9b9a7f330e4950dc400f1b34e5ca9
Joseph Huamani | Nerk <u20221a133@upc.edu.pe>
2024-09-13 20:55:18 -0500

    first commit

---
+++ src/main/java/com/techzo/cambiazo/CambiazoApplication.java
@@ -0,0 +1,13 @@
+package com.techzo.cambiazo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CambiazoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CambiazoApplication.class, args);
+	}
+
+}
