a7c349cbf399e6b4769ca77b04bb319ff3fc4438
123-Mathias <u202216282@upc.edu.pe>
2024-09-15 14:00:45 -0500

    feat: added ExchangeQueryServiceImpl
    I've added the ExchangeQueryServiceImpl class implementing IExchangeQueryService.

---
+++ src/main/java/com/techzo/cambiazo/exchanges/application/internal/queryservices/ExchangeQueryServiceImpl.java
@@ -0,0 +1,56 @@
+package com.techzo.cambiazo.exchanges.application.internal.queryservices;
+
+import com.techzo.cambiazo.exchanges.domain.model.entities.Exchange;
+import com.techzo.cambiazo.exchanges.domain.model.entities.User;
+import com.techzo.cambiazo.exchanges.domain.model.queries.GetAllExchangesByUserChangeIdQuery;
+import com.techzo.cambiazo.exchanges.domain.model.queries.GetAllExchangesByUserOwnIdQuery;
+import com.techzo.cambiazo.exchanges.domain.model.queries.GetAllExchangesQuery;
+import com.techzo.cambiazo.exchanges.domain.model.queries.GetExchangeByIdQuery;
+import com.techzo.cambiazo.exchanges.domain.services.IExchangeQueryService;
+import com.techzo.cambiazo.exchanges.infrastructure.persistence.jpa.IExchangeRepository;
+import com.techzo.cambiazo.exchanges.infrastructure.persistence.jpa.IProductRepository;
+import com.techzo.cambiazo.exchanges.infrastructure.persistence.jpa.IUserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ExchangeQueryServiceImpl implements IExchangeQueryService {
+
+    private final IExchangeRepository exchangeRepository;
+
+    private final IProductRepository productRepository;
+    private final IUserRepository userRepository;
+
+    public ExchangeQueryServiceImpl(IExchangeRepository exchangeRepository, IUserRepository userRepository, IProductRepository productRepository) {
+        this.exchangeRepository = exchangeRepository;
+        this.userRepository = userRepository;
+        this.productRepository = productRepository;
+    }
+
+
+    @Override
+    public Optional<Exchange> handle(GetExchangeByIdQuery query) {
+        return this.exchangeRepository.findById(query.id());
+    }
+
+    @Override
+    public List<Exchange> handle(GetAllExchangesQuery query) {
+        return this.exchangeRepository.findAll();
+    }
+
+    @Override
+    public List<Exchange> handle(GetAllExchangesByUserOwnIdQuery query) {
+        User user = this.userRepository.findById(query.id())
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        return this.exchangeRepository.findAllExchangesByProductOwnId_UserId(user);
+    }
+
+    @Override
+    public List<Exchange> handle(GetAllExchangesByUserChangeIdQuery query) {
+        User user = this.userRepository.findById(query.id())
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        return this.exchangeRepository.findAllExchangesByProductChangeId_UserId(user);
+    }
+}
