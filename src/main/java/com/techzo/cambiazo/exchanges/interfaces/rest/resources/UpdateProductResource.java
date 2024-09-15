845cf98f831e4bf4337d2b5a2826c09a825afbdf
HelloNerk <u20221a133@upc.edu.pe>
2024-09-15 13:13:42 -0500

    feat: added UpdateProductResource
    I've added the UpdateProductResource record to handle updating product input in controllers.

---
+++ src/main/java/com/techzo/cambiazo/exchanges/interfaces/rest/resources/UpdateProductResource.java
@@ -0,0 +1,14 @@
+package com.techzo.cambiazo.exchanges.interfaces.rest.resources;
+
+public record UpdateProductResource(
+        String name,
+        String description,
+        String desiredObject,
+        Double price,
+        String image,
+        Boolean boost,
+        Boolean available,
+        Long productCategoryId,
+        Long userId,
+        Long districtId){
+}
