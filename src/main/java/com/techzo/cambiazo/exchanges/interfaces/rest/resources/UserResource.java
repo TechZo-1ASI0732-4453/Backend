c5127d609e87857a23e6d61e37741692253f74d9
Jemisas <u202216279@upc.edu.pe>
2024-09-15 02:15:55 -0500

    feat: added UserResource
    I've added the UserResource record to handle user output in controllers.

---
+++ src/main/java/com/techzo/cambiazo/exchanges/interfaces/rest/resources/UserResource.java
@@ -0,0 +1,4 @@
+package com.techzo.cambiazo.exchanges.interfaces.rest.resources;
+
+public record UserResource(Long id, String name, String email, String phoneNumber, String password, String profilePicture) {
+}
