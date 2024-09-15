09be3b23f15009a6c0397a75e2b543d4b4d26b9e
HelloNerk <u20221a133@upc.edu.pe>
2024-09-15 14:44:14 -0500

    feat: added ReviewController
    I've added the ReviewController to handle HTTP requests related to review operations.

---
+++ src/main/java/com/techzo/cambiazo/exchanges/interfaces/rest/ReviewController.java
@@ -0,0 +1,71 @@
+package com.techzo.cambiazo.exchanges.interfaces.rest;
+
+import com.techzo.cambiazo.exchanges.domain.model.queries.GetAllReviewsByUserReceptorIdQuery;
+import com.techzo.cambiazo.exchanges.domain.services.IReviewCommandService;
+import com.techzo.cambiazo.exchanges.domain.services.IReviewQueryService;
+import com.techzo.cambiazo.exchanges.interfaces.rest.resources.CreateReviewResource;
+import com.techzo.cambiazo.exchanges.interfaces.rest.resources.ReviewResource;
+import com.techzo.cambiazo.exchanges.interfaces.rest.transform.CreateReviewCommandFromResourceAssembler;
+import com.techzo.cambiazo.exchanges.interfaces.rest.transform.ReviewResourceFromEntityAssembler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequestMapping("/api/v2/reviews")
+@Tag(name = "Reviews", description = "Reviews Management Endpoints")
+public class ReviewController {
+
+    private final IReviewCommandService reviewCommandService;
+
+    private final IReviewQueryService reviewQueryService;
+
+    public ReviewController(IReviewCommandService reviewCommandService, IReviewQueryService reviewQueryService) {
+        this.reviewCommandService = reviewCommandService;
+        this.reviewQueryService = reviewQueryService;
+    }
+
+    @Operation(summary = "Create a new Review", description = "Create a new Review with the input data.")
+    @PostMapping
+    public ResponseEntity<ReviewResource>createReview(@RequestBody CreateReviewResource resource) {
+        try {
+            var createReviewCommand = CreateReviewCommandFromResourceAssembler.toCommandFromResource(resource);
+            var review = reviewCommandService.handle(createReviewCommand);
+            var reviewResource = ReviewResourceFromEntityAssembler.toResourceFromEntity(review.get());
+            return ResponseEntity.status(CREATED).body(reviewResource);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @GetMapping("/user-receptor/{userId}")
+    public ResponseEntity<List<ReviewResource>> getAllReviewsByUserReceptorId(@PathVariable Long userId) {
+        try {
+            var getAllReviewsByUserReceptorIdQuery = new GetAllReviewsByUserReceptorIdQuery(userId);
+            var reviews = reviewQueryService.handle(getAllReviewsByUserReceptorIdQuery);
+            var reviewResource = reviews.stream()
+                    .map(ReviewResourceFromEntityAssembler::toResourceFromEntity)
+                    .toList();
+            return ResponseEntity.ok(reviewResource);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+
+    @DeleteMapping("/delete/{reviewId}")
+    public ResponseEntity<Void> deleteReview(@PathVariable Long reviewId) {
+        try {
+            reviewCommandService.handleDeleteReview(reviewId);
+            return ResponseEntity.noContent().build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+}
