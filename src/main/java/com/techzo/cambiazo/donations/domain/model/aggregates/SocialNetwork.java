ea4d190414c0b60e7690f9459af237d44fccf168
HelloNerk <u20221a133@upc.edu.pe>
2024-09-25 10:45:58 -0500

    feat: added social network methods
    I've added the social network's methods, entity, and controller

---
+++ src/main/java/com/techzo/cambiazo/donations/domain/model/aggregates/SocialNetwork.java
@@ -0,0 +1,75 @@
+package com.techzo.cambiazo.donations.domain.model.aggregates;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import com.techzo.cambiazo.donations.domain.model.commands.CreateSocialNetworkCommand;
+import com.techzo.cambiazo.donations.domain.model.valueobjects.SocialNetworkName;
+import com.techzo.cambiazo.donations.domain.model.valueobjects.SocialNetworkUrl;
+import com.techzo.cambiazo.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+
+/**
+ * Represents a social network entity associated with an NGO.
+ *
+ * @author CambiaZo - TechZo
+ * @version 1.0
+ */
+@Entity
+public class SocialNetwork extends AuditableAbstractAggregateRoot<SocialNetwork> {
+
+    @Embedded
+    @Column(nullable = false)
+    @NotNull(message = "Name is mandatory")
+    private SocialNetworkName name;
+
+    @Embedded
+    @Column(nullable = false)
+    @NotNull(message = "URL is mandatory")
+    private SocialNetworkUrl url;
+
+    @ManyToOne
+    @JoinColumn(name = "ong_id")
+    @NotNull(message = "Ong is mandatory")
+    private Ong ongId;
+
+    public SocialNetwork() {}
+
+    /**
+     * Constructor to create a SocialNetwork object from a command and an Ong entity.
+     *
+     * @param command The create social network command.
+     * @param ong     The Ong entity associated with the social network.
+     */
+    public SocialNetwork(CreateSocialNetworkCommand command, Ong ong) {
+        this.name = new SocialNetworkName(command.name());
+        this.url = new SocialNetworkUrl(command.url());
+        this.ongId = ong;
+    }
+
+    /**
+     * Get the ID of the Ong associated with the social network.
+     *
+     * @return The ID of the Ong.
+     */
+    public Long getOngId() {
+        return ongId.getId();
+    }
+
+    /**
+     * Get the name of the social network.
+     *
+     * @return The name of the social network.
+     */
+    public String getName() {
+        return name.getSocialNetworkName();
+    }
+
+    /**
+     * Get the URL of the social network.
+     *
+     * @return The URL of the social network.
+     */
+    public String getUrl() {
+        return url.getSocialNetworkUrl();
+    }
+
+}
---
+++ src/main/java/com/techzo/cambiazo/donations/interfaces/rest/resources/SocialNetworkResource.java
@@ -0,0 +1,4 @@
+package com.techzo.cambiazo.donations.interfaces.rest.resources;
+
+public record SocialNetworkResource(Long id,String name, String url, Long ongId) {
+}
