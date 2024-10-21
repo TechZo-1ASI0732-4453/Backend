866ccd987466da87ffc3bd18910e0f87e031a868
HelloNerk <u20221a133@upc.edu.pe>
2024-10-21 10:54:37 -0500

    fix: updated product get methods
    I've updated product get methods

--- src/main/java/com/techzo/cambiazo/exchanges/domain/model/dtos/ProductDto.java
+++ src/main/java/com/techzo/cambiazo/exchanges/domain/model/dtos/ProductDto.java
@@ -1,19 +1,20 @@
 package com.techzo.cambiazo.exchanges.domain.model.dtos;
 
 import com.techzo.cambiazo.exchanges.domain.model.entities.*;
-import com.techzo.cambiazo.iam.domain.model.aggregates.User;
-import com.techzo.cambiazo.iam.interfaces.rest.resources.UserResource;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class ProductDto {
-    private Product product;
-    private UserResource user;
-    private ProductCategory productCategory;
-    private District district;
-    private Department department;
-    private Country country;
-
+    private String name;
+    private String description;
+    private String desiredObject;
+    private Double price;
+    private String image;
+    private Boolean boost;
+    private Boolean available;
+    private ProductCategory productCategoryId;
+    private Location location;
 }
+
