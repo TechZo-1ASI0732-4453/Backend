20b52773d75ac2a70d6932674daccdccee8e8fcf
HelloNerk <u20221a133@upc.edu.pe>
2024-10-24 09:30:31 -0500

    feat: added exist review query
    I've added exist review query

---
+++ src/main/java/com/techzo/cambiazo/exchanges/domain/model/dtos/ExistReview.java
@@ -0,0 +1,10 @@
+package com.techzo.cambiazo.exchanges.domain.model.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ExistReview {
+    boolean existReview;
+}
