415e45bd6e32333be2f364d0598f0a974efdde30
HelloNerk <u20221a133@upc.edu.pe>
2024-11-03 13:01:41 -0500

    fix: updated create subscription command
    I've updated create subscription command

--- src/main/java/com/techzo/cambiazo/exchanges/interfaces/rest/resources/SubscriptionResource.java
+++ src/main/java/com/techzo/cambiazo/exchanges/interfaces/rest/resources/SubscriptionResource.java
@@ -1,6 +1,7 @@
 package com.techzo.cambiazo.exchanges.interfaces.rest.resources;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
-public record SubscriptionResource(Long id, String State, Long planId, Long userId, LocalDate startDate, LocalDate endDate) {
+public record SubscriptionResource(Long id, String State, Long planId, Long userId, LocalDateTime startDate, LocalDateTime endDate) {
 }
