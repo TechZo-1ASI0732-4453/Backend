b54795c10d2b35fccb6f3f109a33ee04d6c7319e
HelloNerk <u20221a133@upc.edu.pe>
2024-11-09 03:35:51 -0500

    fix: removedemail methods
    I've removed the email methods

--- src/main/java/com/techzo/cambiazo/emails/services/IEmailService.java
+++
@@ -1,8 +0,0 @@
-package com.techzo.cambiazo.emails.services;
-
-import com.techzo.cambiazo.emails.services.models.EmailDto;
-import jakarta.mail.MessagingException;
-
-public interface IEmailService {
-    public void sendMail(EmailDto emailDto) throws MessagingException;
-}
