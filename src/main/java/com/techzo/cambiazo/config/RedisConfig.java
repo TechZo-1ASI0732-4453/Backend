378fda1caef3ac8233d112533bd6a6244301600a
Jeremy Quispe <134337719+Jemisas@users.noreply.github.com>
2025-11-09 19:06:11 -0500

    feat: added  Jackson config and improve Redis serialization
    Introduced JacksonConfig to customize Jackson's date serialization. Updated RedisConfig to use the new ObjectMapper bean with JavaTimeModule and disabled WRITE_DATES_AS_TIMESTAMPS for proper date handling. Added commons-lang3 dependency to pom.xml.

--- src/main/java/com/techzo/cambiazo/config/RedisConfig.java
+++ src/main/java/com/techzo/cambiazo/config/RedisConfig.java
@@ -1,10 +1,12 @@
 package com.techzo.cambiazo.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.techzo.cambiazo.chat.application.dtos.ChatMessage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -15,9 +17,11 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
 
     @Bean
+    @Primary
     public ObjectMapper redisObjectMapper() {
         ObjectMapper om = new ObjectMapper();
         om.registerModule(new JavaTimeModule());
+        om.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return om.findAndRegisterModules();
     }
 
@@ -29,13 +33,13 @@ public class RedisConfig {
     @Bean
     public RedisTemplate<String, Object> redisTemplate(
             RedisConnectionFactory cf,
-            ObjectMapper om
+            ObjectMapper redisObjectMapper
     ) {
         RedisTemplate<String, Object> tpl = new RedisTemplate<>();
         tpl.setConnectionFactory(cf);
 
         StringRedisSerializer keySer = new StringRedisSerializer();
-        GenericJackson2JsonRedisSerializer valSer = new GenericJackson2JsonRedisSerializer(om);
+        GenericJackson2JsonRedisSerializer valSer = new GenericJackson2JsonRedisSerializer(redisObjectMapper);
 
         tpl.setKeySerializer(keySer);
         tpl.setValueSerializer(valSer);
@@ -49,13 +53,13 @@ public class RedisConfig {
     @Bean
     public RedisTemplate<String, ChatMessage> chatMessageRedisTemplate(
             RedisConnectionFactory cf,
-            ObjectMapper om
+            ObjectMapper redisObjectMapper
     ) {
         RedisTemplate<String, ChatMessage> tpl = new RedisTemplate<>();
         tpl.setConnectionFactory(cf);
 
         StringRedisSerializer keySer = new StringRedisSerializer();
-        GenericJackson2JsonRedisSerializer valSer = new GenericJackson2JsonRedisSerializer(om);
+        GenericJackson2JsonRedisSerializer valSer = new GenericJackson2JsonRedisSerializer(redisObjectMapper);
 
         tpl.setKeySerializer(keySer);
         tpl.setValueSerializer(valSer);
